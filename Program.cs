e6df2d999bfeeba299c77807e1005a01015925b3
José Robles <34249588+roblesdev@users.noreply.github.com>
2021-06-10 23:35:04 -0400

    Comparando Numeros

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
-namespace primerProyecto
+namespace ejercicios2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Probando mi Primer Proyecto");
+
+            Console.WriteLine("Ingresar Primer Número");
+            int primerNumero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresar Segundo Número");
+            int segundoNumero = int.Parse(Console.ReadLine());
+
+            if(primerNumero < segundoNumero)
+            {
+            Console.WriteLine("Primer número ingresado es menor que el segundo");
+
+            }
+            else if (primerNumero > segundoNumero)
+            {
+                Console.WriteLine("Primer número ingresado es mayor que el segundo");
+
+            }
+            else
+            {
+                Console.WriteLine("Ambos números son iguales");
+
+            }
         }
     }
 }
